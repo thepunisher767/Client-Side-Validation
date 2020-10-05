3261ca4e9083063a7325d4d3a235911f9a8dd671
zscholts <zachscholts@quickenloans.com>
2020-10-05 17:20:48 -0400

    initial

---
+++ Client-Side Validation Lab/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Client_Side_Validation_Lab.Models;
+
+namespace Client_Side_Validation_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GetUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Submit(string firstname, string lastname, string email, string password)
+        {
+            User user = new User() { firstName = firstname, lastName = lastname, email = email, password = password };
+            return View(user);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Client-Side Validation Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Client_Side_Validation_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
